6539f524965194fc4b949b7e1d8a6a5b4a48b79a
Camila Borges <borgescamila09@gmail.com>
2025-08-13 21:46:47 -0400

    dia 1

---
+++ javascript-para-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,56 @@
+var nome = 'André';// String
+var idade = 28;// Number
+var possuiFaculdade = true;// Boolean
+var time;// Undefined
+var comida = null;// Null
+var simbolo = Symbol()// Symbol
+var novoObjeto = {}// Object
+
+var nome = 'André';
+console.log(typeof nome);
+// retorna string
+
+var nome = 'André';
+var sobrenome = 'Rafael';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+var gols = 1000;
+var frase = 'Romário fez ' + gols + ' gols';
+
+
+'JavaScript é "super" fácil';
+"JavaScript é 'super' fácil";
+"JavaScript é \"super\" fácil";
+`JavaScript é "super" fácil"`;
+// "JavaScript é "super" fácil";// Inválido
+
+var gols = 1000;
+var frase1 = 'Romário fez ' + gols + ' gols';
+var frase2 = `Romário fez ${gols} gols`;// Utilizando Template String
+
+// Declare uma variável contendo uma string
+var nome = "Camila"
+console.log(nome);
+
+// Declare uma variável contendo um número dentro de uma string
+var idade = "20"
+console.log(idade);
+
+// Declare uma variável com a sua idade
+var minhaIdade = 20
+console.log(minhaIdade);
+
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var meuNome = "Camila "
+var sobrenome = "Borges"
+console.log(meuNome + sobrenome);
+
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's timeee!"
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
---
+++ javascript-para-iniciantes/variaveis/script.js
@@ -0,0 +1,36 @@
+var nome = "Camila"
+var idade = 20
+var possuiFacul = true
+console.log(nome, idade, possuiFacul)
+
+var preco = 25
+var totalComprado = 5
+var totalPreco = preco * totalComprado
+console.log(totalPreco)
+
+var sobrenome = "Joao",
+  cidade = "Toronto"
+console.log(sobrenome, cidade)
+
+var precoIndefinido
+console.log(precoIndefinido)
+
+// Atividade da aula 1
+let meuNome = "Camila Borges"
+let minhaIdade = 20
+let minhaComidaFavorita
+minhaComidaFavorita = "Hamburguer"
+let variavel1
+let variavel2
+let variavel3
+let variave           l4
+let variavel5
+
+console.log(meuNome)
+console.log(minhaIdade)
+console.log(minhaComidaFavorita)
+console.log(variavel1)
+console.log(variavel2)
+console.log(variavel3)
+console.log(variavel4)
+console.log(variavel5)
