9bf7a54452c1dedaed858f11dfaa77d3e7ba473f
CAMILA BORGES VENANCIO <05779088209@IDARON.GOV>
2025-08-18 08:05:17 -0400

    update

--- javascript-para-iniciantes/boolean-e-condicionais/script.js
+++ javascript-para-iniciantes/boolean-e-condicionais/script.js
@@ -2,5 +2,21 @@ var possuiGraduacao = true
 
 if(true){
   console.log('É verdadeiro');
+}
+
+if(possuiGraduacao){
+  console.log("Possui graduação");
+} else {
+  console.log("Não possui graduação");
+}
+
+var possuiDoutorado = true
+
+if(possuiDoutorado){
+  console.log("Possui graduação e doutorado");
+} else if (possuiGraduacao) {
+  console.log("Possui graduação");
+} else {
+  console.log("Não possui nada");
   
 }
