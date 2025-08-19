2ad87549459b0ceff8cb77291d0b6072c2b671f3
CAMILA BORGES VENANCIO <05779088209@IDARON.GOV>
2025-08-19 10:10:46 -0400

    dia 6

--- javascript-para-iniciantes/funcoes/script.js
+++ javascript-para-iniciantes/funcoes/script.js
@@ -1,14 +1,73 @@
-function sum (a, b){
+function sum(a, b) {
   return a + b
 }
 console.log(sum(2, 3));
 
-function imc(peso, altura){
+function imc(peso, altura) {
   var imc = peso / (altura * altura)
   return imc
 }
 console.log(imc(56, 1.68))
 
-addEventListener('click', function(){console.log("Clicouu");
+addEventListener('click', function () {
+  console.log("Clicouu");
 })
 
+// Crie uma função para verificar se um valor é Truthy
+function isTruthy(dado) {
+  return !!dado
+}
+
+// Crie uma função matemática que retorne o perímetro de um quadrado
+// lembrando: perímetro é a soma dos quatro lados do quadrado
+
+function perimetro(lado1) {
+  var perimetro = lado1 * 4
+  return perimetro
+}
+
+// Crie uma função que retorne o seu nome completo
+// ela deve possuir os parâmetros: nome e sobrenome
+
+function nomeCompleto(nome, sobrenome) {
+  return nome + " " + sobrenome
+}
+
+
+// Crie uma função que verifica se um número é par
+function isEven(number) {
+  if (number == 0 || number % 2 == 0) {
+    return "Par"
+  } else {
+    return "Ímpar"
+  }
+}
+
+// Crie uma função que retorne o tipo de
+// dado do argumento passado nela (typeof)
+
+function type(valor) {
+  return typeof valor;
+}
+
+// addEventListener é uma função nativa do JavaScript
+// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
+// utilize essa função para mostrar no console o seu nome completo
+// quando o evento 'click' ocorrer.
+addEventListener('click', function () {
+  console.log('Camila');
+})
+
+// Corrija o erro abaixo
+var totalPaises = 193;
+
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+
+function jaVisitei(paisesVisitados) {
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+
+console.log(precisoVisitar(20))
+console.log(jaVisitei(20))
