7d585512fdfd06ff3ea1e5376919c3b661caf05a
CAMILA BORGES VENANCIO <05779088209@IDARON.GOV>
2025-08-20 12:02:12 -0400

    Day 7

--- javascript-para-iniciantes/tudo-e-objeto/script.js
+++ javascript-para-iniciantes/tudo-e-objeto/script.js
@@ -17,3 +17,42 @@ function areaQuadrado(lado){
 console.log(areaQuadrado.toString())
 console.log(areaQuadrado.length)
 
+// Elementos do DOM
+//Existe um obj chamado document
+ var btn = document.querySelector('.btn') 
+
+ // Para selecionar uma tag ou elemento dentro do HTML
+ // O argumento passado dentro do query é um seletor CSS
+
+ // O elemento do DOM possui métodos e propriedades também
+
+ btn.addEventListener('click', function(){
+  console.log('botão');
+ })
+
+console.log(btn.classList)
+// Vai listar todas as classes 
+
+console.log(btn.classList.add('ativo'));
+// Adiciona uma classe 
+
+console.log(btn.innerText);
+// Vai mostrar o nome, o que está escrito entre as tags
+
+// Comunicação entre duas máquinas, código com código = api
+
+
+// nomeie 3 propriedades ou métodos de strings
+var name = "João Wesley"
+console.log(name.length);
+console.log(name.toLowerCase());
+console.log(name.replace('João', 'Camila'));
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var paragraph = document.querySelector('.paragraph')
+console.log(paragraph.classList)
+console.log(paragraph.classList.add('teste'))
+console.log(paragraph.innerHTML)
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
