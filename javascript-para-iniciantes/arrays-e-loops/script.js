3842896c6754188d5caa6148da4e808c46c944b7
Camila Borges <borgescamila09@gmail.com>
2025-08-20 21:19:14 -0400

    Day 7

--- javascript-para-iniciantes/arrays-e-loops/script.js
+++ javascript-para-iniciantes/arrays-e-loops/script.js
@@ -2,7 +2,7 @@ var videoGames = ['Switch', 'PS4', 'XBOX']
 console.log(videoGames[2]);
 // Vai mostrar o segundo elemento do array, lembrando que começa a contar do 0
 
-console.log(videoGames.pop);
+console.log(videoGames.pop());
 //Remove o último array e retorna
 
 console.log(videoGames.push('3DS'));
@@ -12,10 +12,69 @@ console.log(videoGames.length);
 // Vê o tamanho do Array
 
 
-for(var numero = 0; numero < 10; numero++){
+for (var numero = 0; numero < 5; numero++) {
   console.log(numero);
 }
 // Primeiro criamos a variável com o valor inicial, depois colocamos a condição e depois o incremento
 
+var i = 0
+while (i <= 20) {
+  i = i + 5
+  console.log(i);
+}
 
+for (let item = 0; item <= videoGames.length; item++) {
+  console.log(videoGames[item]);
+}
 
+// We can use 'break' to stop the loop
+
+for (let item = 0; item <= videoGames.length; item++) {
+  if (videoGames[item] === 'XBOX') {
+    break
+  }
+  console.log(videoGames[item]);
+}
+
+let frutinhas = ['Banana', 'Limão', 'Uva', 'Abacate']
+
+frutinhas.forEach((fruta, index, array) => {
+  console.log(fruta, index, array);
+})
+
+// Para o forEach, ele vai fazer uma ação para cada elemento do array. Ele sempre volta uma callback, dentro dessa callback podemos chamar 3 parâmetros: o valor de cada elemento do array, o índice de cada elemento e o array em si
+
+// Não é necessário ter o valor inicial e o incremento no forLoop, se tiver a condição ele já funciona
+
+var numeroInicial = 0
+var numeroFinal = 10
+for (; numeroInicial <= numeroFinal;) {
+  console.log(numeroInicial);
+  numeroInicial = numeroInicial + 1
+}
+
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+var brasilCopa = [1959, 1962, 1970, 1994, 2002]
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+
+for (let i = 0; i < brasilCopa.length; i++) {
+  console.log('O Brasil ganhou a copa de ', brasilCopa[i]);
+}
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+for (let i = 0; i < frutas.length; i++) {
+  if (frutas[i] === 'Pera') {
+    break
+  }
+  console.log(frutas[i]);
+}
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+
+var ultimaFruta = frutas[4]
+console.log(ultimaFruta);
--- javascript-para-iniciantes/funcoes/script.js
+++ javascript-para-iniciantes/funcoes/script.js
@@ -14,7 +14,6 @@ addEventListener('click', function () {
 })
 
 // Crie uma função para verificar se um valor é Truthy
-<<<<<<< HEAD
 function verificaTruthy(valor) {
   return value
   // if (value) {
@@ -39,7 +38,7 @@ console.log();
 // ela deve possuir os parâmetros: nome e sobrenome
 
 // Crie uma função que verifica se um número é par
-=======
+
 function isTruthy(dado) {
   return !!dado
 }
@@ -68,31 +67,11 @@ function isEven(number) {
     return "Ímpar"
   }
 }
->>>>>>> 5b4832c0ab7fb0a1fe89d9ea72d736980486c0c1
 
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
 
-<<<<<<< HEAD
-// addEventListener é uma função nativa do JavaScript
-// o primeiro parâmetro é o evento que ocorre e o segundo o Callback
-// utilize essa função para mostrar no console o seu nome completo
-// quando o evento 'scroll' ocorrer.
 
-// Corrija o erro abaixo
-
-// function precisoVisitar(paisesVisitados) {
-//   var totalPaises = 193;
-//   return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
-// }
-// function jaVisitei(paisesVisitados) {
-//   return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
-// }
-// precisoVisitar(20);
-// jaVisitei(20);
-
-
-=======
 function type(valor) {
   return typeof valor;
 }
@@ -101,6 +80,7 @@ function type(valor) {
 // o primeiro parâmetro é o evento que ocorre e o segundo o Callback
 // utilize essa função para mostrar no console o seu nome completo
 // quando o evento 'click' ocorrer.
+
 addEventListener('click', function () {
   console.log('Camila');
 })
@@ -118,4 +98,4 @@ function jaVisitei(paisesVisitados) {
 
 console.log(precisoVisitar(20))
 console.log(jaVisitei(20))
->>>>>>> 5b4832c0ab7fb0a1fe89d9ea72d736980486c0c1
+
