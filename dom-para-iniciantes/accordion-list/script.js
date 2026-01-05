a556fa183c188e1abfedaa8655928ac21eb0a184
CAMILA BORGES VENANCIO <05779088209@IDARON.GOV>
2026-01-05 12:18:22 -0400

    accordion list

--- dom-para-iniciantes/accordion-list/script.js
+++ dom-para-iniciantes/accordion-list/script.js
@@ -1,27 +1,38 @@
-// A ideia é que haja duas listas, quando você interage com um elemento da primeira lista ela se relaciona com um conteúdo da outra lista
 
-// No html adicionamos uma outra classe para que possamos usar ela para manipular por aqui, pois pode acontecer de usar a outra classe e ela bater com a classe de estilização do elemento. Vamos padronizar essas tags colocando elas sempre com o começo "js"
+function activeTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li')
+    const tabContent = document.querySelectorAll('.js-tabcontent section')
+    tabContent[0].classList.add('ativo')
 
-// Primeiro selecionamos os elementos específico aos quais iremos aplicar o loop 
+    if (tabContent.length && tabMenu.length)
+        function activeTab(index) {
+            tabContent.forEach((item) => {
+                item.classList.remove("ativo")
+            });
+            tabContent[index].classList.add("ativo")
+        }
 
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll('.js-tabcontent section')
-tabContent[0].classList.add('ativo')
+    tabMenu.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            activeTab(index)
+        })
+    });
+}
+activeTabNav()
 
-// Depois vamos criar uma função para ativar as tabs de acordo com sua posição no index
-
-if (tabContent.length && tabMenu.length)
-    function activeTab(index) {
-        tabContent.forEach((item) => {
-            item.classList.remove("ativo")
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt')
+    accordionList[0].classList.add('ativo')
+    accordionList[0].nextElementSibling.classList.add('ativo')
+    
+    if (accordionList.length) {
+        function activeAccordion() {
+            this.nextElementSibling.classList.toggle("ativo")
+            this.classList.toggle('ativo')
+        }
+        accordionList.forEach((item) => {
+            item.addEventListener("click", activeAccordion)
         });
-        tabContent[index].classList.add("ativo")
     }
-
-tabMenu.forEach((item, index) => {
-    item.addEventListener("click", () => {
-        activeTab(index)
-    })
-});
-
-// Agora é alterar nosso CSS para que ele esconda o conteúdo que não estiver com a classe ativo adicionada
+}
+initAccordion()
