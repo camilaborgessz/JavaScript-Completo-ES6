346c8f14dbe5e441c5c45c27b6ebb67171d590f5
Camila Borges <borgescamila09@gmail.com>
2026-02-04 21:11:30 -0400

    tooltip

---
+++ efeitos-no-dom/tooltip/js/modules/animacao-scroll.js
@@ -0,0 +1,20 @@
+export default function animacaoScroll() {
+    const sections = document.querySelectorAll('[data-anime="scroll"]')
+    const metadeTela = window.innerHeight * 0.6
+
+    function animacao() {
+        sections.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top 
+            const isSectionVisible = (sectionTop - metadeTela) < 0
+
+            if(isSectionVisible){
+                section.classList.add('ativo')
+            } else {
+                section.classList.remove('ativo')
+            }
+        });
+    }
+
+    animacao()
+    window.addEventListener('scroll', animacao)
+}
---
+++ efeitos-no-dom/tooltip/js/script.js
@@ -0,0 +1,13 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import animacaoScroll from "./modules/animacao-scroll.js";
+import activeTabNav from "./modules/tab-nav.js";
+import initAccordion from "./modules/accordion.js";
+import initModal from "./modules/modal.js";
+
+initScrollSuave();
+animacaoScroll();
+activeTabNav();
+initAccordion();
+initModal();
+initTooltip();
+
