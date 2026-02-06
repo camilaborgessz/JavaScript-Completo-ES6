7b7559ab549fe45beea69604465836ff5a98e3c8
Camila Borges <borgescamila09@gmail.com>
2026-02-05 22:17:13 -0400

    dropdown menu

---
+++ efeitos-no-dom/dropdown-menu/js/script.js
@@ -0,0 +1,16 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import animacaoScroll from "./modules/animacao-scroll.js";
+import activeTabNav from "./modules/tab-nav.js";
+import initAccordion from "./modules/accordion.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropDown from "./modules/dropdown.js";
+
+initScrollSuave();
+animacaoScroll();
+activeTabNav();
+initAccordion();
+initModal();
+initTooltip();
+initDropDown();
+
