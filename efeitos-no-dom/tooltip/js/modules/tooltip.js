3675c25767b462a17975fe6fb614d2d362097b73
Camila Borges <borgescamila09@gmail.com>
2026-02-05 21:38:35 -0400

    tooltip

--- efeitos-no-dom/tooltip/js/modules/tooltip.js
+++ efeitos-no-dom/tooltip/js/modules/tooltip.js
@@ -1,11 +1,43 @@
 export default function initTooltip() {
-  const mapa = document.querySelector('[data-tooltip]')
-  
-    addTooltip(event){
-        console.log(event.target)
+  const tooltips = document.querySelectorAll('[data-tooltip]')
+
+  tooltips.forEach(item => {
+    item.addEventListener('mouseover', onMouseOver)
+  });
+
+
+  function onMouseOver(event) {
+    this.addEventListener('mousemove', onMouseMove)
+    const tooltipBox = criarTooltipBox(this) 
+    onMouseMove.tooltipBox = tooltipBox
+    onMouseMove.element = this
+
+    onMouseLeave.element = this
+    onMouseLeave.tooltipBox = tooltipBox
+    this.addEventListener('mouseleave', onMouseLeave)
+  }
+
+  function criarTooltipBox(element) {
+    const tooltipBox = document.createElement('div')
+    const text = element.getAttribute('aria-label')
+    tooltipBox.classList.add('tooltip')
+    tooltipBox.innerText = text
+    document.body.appendChild(tooltipBox)
+    return tooltipBox
+  }
+
+  const onMouseMove = {
+    handleEvent(event){
+      this.tooltipBox.style.top = event.pageY + 20 +'px'
+      this.tooltipBox.style.left = event.pageX + 20 + 'px'
+      this.element.removeEventListener('mouseleave', onMouseMove)
     }
+  }
 
-
-  mapa.addEventListener('mouseover', addTooltip)
+  const onMouseLeave = {
+    handleEvent() {
+      this.tooltipBox.remove()
+      this.element.removeEventListener('mouseleave', onMouseLeave)
+    }
+  }
 }
-
