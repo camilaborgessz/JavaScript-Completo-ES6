6085ed7cf13344bc54ac2c56f4f5292149adb5b0
Camila Borges <borgescamila09@gmail.com>
2026-02-09 22:40:48 -0400

    update

--- efeitos-no-dom/dropdown-menu/js/modules/dropdown.js
+++ efeitos-no-dom/dropdown-menu/js/modules/dropdown.js
@@ -1,15 +1,30 @@
 
 export default function initDropDown() {
   const dropdown = document.querySelectorAll('[data-dropdown]')
-
-  function openDropdown(event){
-    event.preventDefault()
-    this.classList.toggle('active')
-  }
-
+  
+  
   dropdown.forEach(element => {
-    ['touchstart', 'click' ].forEach(userEvent => {
+    ['touchstart', 'click'].forEach(userEvent => {
       element.addEventListener(userEvent, openDropdown)
     });
   });
+  
+  function openDropdown(event) {
+    event.preventDefault()
+    this.classList.toggle('active')
+    outsideClick(this ,() => {
+      console.log(this);
+      
+    })
+  }
+  
+  function outsideClick(element, callback) {
+    const html = document.documentElement
+    html.addEventListener('click', handleOutsideClick)
+    function handleOutsideClick(event){
+      callback()
+    }
+  }
+
+
 }
